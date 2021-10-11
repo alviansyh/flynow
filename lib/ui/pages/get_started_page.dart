0bd24e5eb334a537e3fc54b6165672844d1d9b55
Muhammad Alviansyah <workalvin21@gmail.com>
2021-10-11 22:32:01 +0700

    [ADD] assets, theme [SLICING DESIGN] splash page, get started

---
+++ lib/ui/pages/get_started_page.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import '../../shared/theme.dart';
+
+class GetStartedPage extends StatelessWidget {
+  const GetStartedPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/image_get_started.png',
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  'It’s Time To Fly',
+                  style: whiteTextStyle.copyWith(
+                    fontSize: 32,
+                    fontWeight: semiBold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Explore new world with us and let \nyourself get an amazing experiences',
+                  style: whiteTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: light,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.075,
+                  width: MediaQuery.of(context).size.width * 0.60,
+                  margin: const EdgeInsets.only(
+                    top: 50,
+                    bottom: 50,
+                  ),
+                  child: TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      backgroundColor: kPrimaryColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    child: Text(
+                      'Get Started',
+                      style: whiteTextStyle.copyWith(
+                        fontSize: 18,
+                        fontWeight: medium,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/ui/pages/splash_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+import '../../shared/theme.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Transform.rotate(
+              angle: math.pi / 4,
+              child: Container(
+                width: 100,
+                height: 100,
+                margin: const EdgeInsets.only(bottom: 50),
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                      'assets/icon_plane.png',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Text(
+              'FLYNOW',
+              style: whiteTextStyle.copyWith(
+                fontSize: 32,
+                fontWeight: medium,
+                letterSpacing: 15,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
